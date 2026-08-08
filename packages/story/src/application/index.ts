e702a29eb20cf0d3ee9202088f6a4be8c1cc2431
necdetoskay <necdetoskay@hotmail.com>
2026-08-08 13:30:30 +0300

    feat(story): export generated scene session bridge

--- packages/story/src/application/index.ts
+++ packages/story/src/application/index.ts
@@ -1,92 +1,3 @@
-export {
-  createStoryDefinition,
-  createStoryVersion,
-  saveSceneGraph,
-  publishStoryVersion,
-  getStoryCatalog,
-  ensureStarterStoriesForHousehold,
-  getStoryVersionGraph,
-  getStoryVersionGraphByNumber,
-  getStoryDefinitionById,
-  getStoryVersionById,
-  __setTestDefinitionDb,
-} from "./story-definition.service";
-export type {
-  CreateStoryDefinitionServiceInput,
-  CreateStoryVersionServiceInput,
-  SceneGraphInput,
-  TransitionGraphInput,
-  SaveSceneGraphInput,
-} from "./story-definition.service";
-
-export {
-  startSession,
-  pauseSession,
-  resumeSession,
-  advanceSession,
-  completeSession,
-  abandonSession,
-  getSessionPlaybackState,
-  getSessionHistory,
-  getLatestCheckpoint,
-  createManualCheckpoint,
-  getSessionById,
-  getActiveSessionForChildAndWorld,
-  listSessionsForChildProfile,
-  __setTestSessionDb,
-} from "./story-session.service";
-export type {
-  StartSessionInput,
-  SessionStateChangeInput,
-  AdvanceSessionInput,
-  AbandonSessionInput,
-} from "./story-session.service";
-
-export {
-  assertStorySessionAccess,
-  getStorySessionOrForbidden,
-  __setTestAuthDb,
-} from "./story-auth.service";
-
-export {
-  recordStoryEvent,
-  recordStoryEventWithTx,
-  getStoryEvents,
-  getStoryEventCountByType,
-  __setTestEventDb,
-} from "./story-event-store.service";
-export type { RecordStoryEventInput } from "./story-event-store.service";
-
-export {
-  createChoicePoint,
-  getChoicePointWithOptions,
-  listChoicePointsByScene,
-  listChoicePointsByVersion,
-  evaluateChoicePointAvailability,
-  commitChoice,
-  getChoiceHistory,
-  createOutcomeCandidate,
-  getLatestOutcomeCandidate,
-  listConsequencesBySession,
-  __setTestChoiceDb,
-} from "./choice/choice.service";
-export type {
-  CreateChoicePointServiceInput,
-  CreateChoiceOptionServiceInput,
-  CommitChoiceInput,
-} from "./choice/choice.service";
-
-export {
-  evaluateRule,
-  evaluateOptionAvailability,
-} from "./choice/rule-evaluator";
-export { hashObject } from "./hash";
-
-export {
-  WorldCommitService,
-  __setTestCommitDb,
-  commitOutcomeWithTx,
-} from "./world-commit.service";
 export type {
   CommitManifestInput,
   CommitResult,
@@ -145,3 +56,12 @@ export type {
   OpenRouterCallResult,
   OpenRouterMessage,
 } from "./story-scene-generation.service";
+
+export {
+  persistGeneratedSceneAndAdvance,
+  __setTestGeneratedSceneDb,
+} from "./generated-scene-session.service";
+export type {
+  PersistGeneratedSceneAndAdvanceInput,
+  PersistGeneratedSceneAndAdvanceResult,
+} from "./generated-scene-session.service";
