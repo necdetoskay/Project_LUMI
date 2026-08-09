e71267fb5b7d50653110690b8b70bbae0062e11c
necdetoskay <necdetoskay@hotmail.com>
2026-08-09 11:56:46 +0300

    feat(s37): export scoped hook consumption boundary

--- packages/story/src/application/index.ts
+++ packages/story/src/application/index.ts
@@ -119,6 +119,11 @@ export { StoryHookDeliveryApplicator } from "./story-hook-delivery-applicator.se
 
 export { StoryHookService } from "./story-hook.service";
 export type { StoryHookResult } from "./story-hook.service";
+export {
+  getStoryHookForConsumption,
+  markStoryHookConsumed,
+} from "./story-hook-consumption.service";
+export type { StoryHookConsumptionScope } from "./story-hook-consumption.service";
 
 export {
   enqueueQuestRewardIntent,
