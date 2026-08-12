20c94704ff7d60396cc72496c6f3e7277776b66a
necdetoskay <necdetoskay@hotmail.com>
2026-08-12 21:28:27 +0300

    feat(media): export story visual workspace read model

--- packages/media/src/application/index.ts
+++ packages/media/src/application/index.ts
@@ -2,4 +2,5 @@ export * from "./audio-pipeline.service";
 export * from "./cost-estimator.service";
 export * from "./image-pipeline.service";
 export * from "./policy-enforcer.service";
+export * from "./story-visual-workspace.service";
 export * from "./visual-prompt-compiler";
