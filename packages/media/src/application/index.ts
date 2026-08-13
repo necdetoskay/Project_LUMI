f5cddce25ccc44907eb06b597e46960bbedfe170
necdetoskay <necdetoskay@hotmail.com>
2026-08-13 07:59:24 +0300

    fix(media): alias generation asset sheet public type

--- packages/media/src/application/index.ts
+++ packages/media/src/application/index.ts
@@ -1,3 +1,13 @@
+export {
+  planStoryVisualAssetSheets,
+  STORY_VISUAL_SHEET_GRID_COLUMNS,
+  STORY_VISUAL_SHEET_GRID_ROWS,
+  STORY_VISUAL_SHEET_MAX_CELLS,
+  STORY_VISUAL_SHEET_OUTPUT_MAX_PX,
+  type AssetSheetCandidate,
+  type AssetSheetCell,
+  type StoryVisualAssetSheetPlan as GenerationAssetSheetPlan,
+} from "./asset-sheet-planner";
 export * from "./audio-pipeline.service";
 export * from "./cost-estimator.service";
 export * from "./image-pipeline.service";
