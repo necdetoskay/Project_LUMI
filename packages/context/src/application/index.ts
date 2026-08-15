cd0834d397d9bb5f6527d045d8a75d071f67d580
necdetoskay <necdetoskay@hotmail.com>
2026-08-15 11:56:00 +0300

    feat(context): export story generation context composer

--- packages/context/src/application/index.ts
+++ packages/context/src/application/index.ts
@@ -11,3 +11,11 @@ export {
   originPackageToItems,
 } from "./context-builder";
 export type { ContextBuilderDeps } from "./context-builder";
+
+export {
+  STORY_GENERATION_CONTEXT_TOKENS,
+  STORY_GENERATION_TOKEN_BUDGET,
+  StoryGenerationContextComposer,
+  createStoryGenerationContextComposer,
+} from "./story-generation-context";
+export type { StoryGenerationContextRequest } from "./story-generation-context";
