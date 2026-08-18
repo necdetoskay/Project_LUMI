1a9fd98af616b1dd73174d9e9ac38736efaf03b1
necdetoskay <necdetoskay@hotmail.com>
2026-08-18 08:36:38 +0300

    feat(context): export inspector read service

--- packages/profiles/src/index.ts
+++ packages/profiles/src/index.ts
@@ -4,6 +4,7 @@ export * from "./policy";
 export * from "./application";
 export * from "./application/generation-context-source";
 export * from "./application/generation-context-compaction";
+export * from "./application/ai-generation-trace.service";
 export * from "./application/creative-genesis-pipeline.service";
 export * from "./application/saga-foundation.service";
 export * from "./application/onboarding-foundation-commit.service";
