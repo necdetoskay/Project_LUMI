38d1df453378155a64a686ce8c352698c2a2c581
necdetoskay <necdetoskay@hotmail.com>
2026-08-20 07:54:47 +0300

    feat(genesis): export validation context port

--- packages/world/src/application/index.ts
+++ packages/world/src/application/index.ts
@@ -21,6 +21,7 @@ export type {
   CharacterGenesisCanonicalCommitPort,
   CharacterGenesisCanonicalCommitRequest,
   CharacterGenesisCanonicalCommitResult,
+  CharacterGenesisValidationContextPort,
   CommitCharacterGenesisResult,
 } from "./character-genesis.service";
 
