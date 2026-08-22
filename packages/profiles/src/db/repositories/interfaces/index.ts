5530e76663d127ea386dc559e0bf5fad94966567
necdetoskay <necdetoskay@hotmail.com>
2026-08-22 07:51:24 +0300

    feat(profiles): export child avatar repository contract

--- packages/profiles/src/db/repositories/interfaces/index.ts
+++ packages/profiles/src/db/repositories/interfaces/index.ts
@@ -3,6 +3,7 @@ export type { ChildProfileRepository } from "./child-profile.repository";
 export type { ParentPolicyRepository } from "./parent-policy.repository";
 export type { FirstRunHandoffRepository } from "./first-run-handoff.repository";
 export type { CharacterRepository } from "./character.repository";
+export type { ChildAvatarRepository } from "./child-avatar.repository";
 export type { CharacterOriginPackageRepository } from "./character-origin-package.repository";
 export type { HandoffConsumptionRepository } from "./handoff-consumption.repository";
 export type { CharacterDomainRepository } from "./character-domain.repository";
