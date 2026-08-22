46ec943d8ca281df918a17b715346a54335077a3
necdetoskay <necdetoskay@hotmail.com>
2026-08-22 07:52:17 +0300

    feat(world): export npc repository contract

--- packages/world/src/db/repositories/interfaces/index.ts
+++ packages/world/src/db/repositories/interfaces/index.ts
@@ -1,3 +1,4 @@
 export type { WorldRepository } from "./world.repository";
+export type { NpcRepository } from "./npc.repository";
 export type { QuestRepository } from "./quest.repository";
 export type { QuestTemplateRepository } from "./quest-template.repository";
