1776e8ae2ccf2ef71ac31ad6d196153008a8d483
necdetoskay <necdetoskay@hotmail.com>
2026-08-22 08:13:07 +0300

    refactor(world): export NPC repository

--- packages/world/src/db/repositories/drizzle/index.ts
+++ packages/world/src/db/repositories/drizzle/index.ts
@@ -1,4 +1,5 @@
 export { DrizzleWorldRepository } from "./drizzle-world.repository";
+export { DrizzleNpcRepository } from "./drizzle-npc.repository";
 export { DrizzleQuestRepository } from "./drizzle-quest.repository";
 export { DrizzleQuestTemplateRepository } from "./drizzle-quest-template.repository";
 export {
